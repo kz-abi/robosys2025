1076fffea80f9d782a80ce8e79fb139ab5e52021
kz_abi <mikazuki503@gmail.com>
2025-11-30 08:07:30 +0900

    Update test script for resistor

--- test.bash
+++ test.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -xv
-# SPDX-FileCopyrightText: 2025 Ryuichi Ueda 　　　　　
+# SPDX-FileCopyrightText: 2025 kz-abi 　　　　　
 # SPDX-License-Identifier: BSD-3-Clause
 ng () {
 	echo ${1}行目が違うよ	
@@ -8,15 +8,18 @@ ng () {
 
 res=0
 
-out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng "$LINENO"
-out=$(echo あ | ./plus)
-[ "$?" = 1 ]      || ng "$LINENO"
-[ "${out}" = "" ] || ng "$LINENO"
+# 100 -> 茶 黒 茶
+out=$(echo "100" | ./resistor)
+[ "${out}" = "🟤 ⬛ 🟤" ] || ng "$LINENO"
 
-out=$(echo | ./plus)
-[ "$?" = 1 ]      || ng "$LINENO"
-[ "${out}" = "" ] || ng "$LINENO"
+# 4.7k -> 黄 紫 赤
+out=$(echo "4.7k" | ./resistor)
+[ "${out}" = "🟡 🟣 🔴" ] || ng "$LINENO"
 
-[ "${res}" = 0 ] && echo OK
+# 異常値
+out=$(echo "あ" | ./resistor)
+[ "${out}" = "数値を入力してください" ] || ng "$LINENO"
+
+[ "$res" = 0 ] && echo OK
 exit $res
+
