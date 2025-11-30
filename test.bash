49e471e5e31ceca32576a98c10533009989d9066
kz_abi <mikazuki503@gmail.com>
2025-11-30 10:23:22 +0900

    Update test script

--- test.bash
+++ test.bash
@@ -1,25 +1,25 @@
-#!/bin/bash -xv
-# SPDX-FileCopyrightText: 2025 kz-abi 　　　　　
+#!/bin/bash
+# SPDX-FileCopyrightText: 2025 kz-abi
 # SPDX-License-Identifier: BSD-3-Clause
+
 ng () {
-	echo ${1}行目が違うよ	
-	res=1
+    echo ${1}行目が違うよ
+    res=1
 }
 
 res=0
 
-# 100 -> 茶 黒 茶
+# 正方向テスト1: 100 Ω -> 茶 黒 茶
 out=$(echo "100" | ./resistor)
-[ "${out}" = "🟤 ⬛ 🟤" ] || ng "$LINENO"
+[ "${out}" = "🟤 ⬛ 🟤 (茶 黒 茶)" ] || ng "$LINENO"
 
-# 4.7k -> 黄 紫 赤
+# 正方向テスト2: 4.7k Ω -> 黄 紫 赤
 out=$(echo "4.7k" | ./resistor)
-[ "${out}" = "🟡 🟣 🔴" ] || ng "$LINENO"
+[ "${out}" = "🟡 🟣 🔴 (黄 紫 赤)" ] || ng "$LINENO"
 
-# 異常値
+# 異常値テスト
 out=$(echo "あ" | ./resistor)
 [ "${out}" = "数値を入力してください" ] || ng "$LINENO"
 
 [ "$res" = 0 ] && echo OK
 exit $res
-
