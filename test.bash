f99955ae5d335423c5534da254e0b062f2193d8c
kz_abi <mikazuki503@gmail.com>
2025-12-10 10:51:40 +0900

    Fix test script

--- test.bash
+++ test.bash
@@ -10,16 +10,18 @@ ng () {
 res=0
 
 # 正方向テスト1: 100 Ω -> 茶 黒 茶
-out=$(echo "100" | ./resistor)
-[ "${out}" = "🟤 ⬛ 🟤 (茶 黒 茶)" ] || ng "$LINENO"
+$(echo "100" | ./resistor)
+[ "${out}" = "🟤 ⬛ 🟤 : 茶 黒 茶" ] || ng "$LINENO"
 
 # 正方向テスト2: 4.7k Ω -> 黄 紫 赤
 out=$(echo "4.7k" | ./resistor)
-[ "${out}" = "🟡 🟣 🔴 (黄 紫 赤)" ] || ng "$LINENO"
+[ "${out}" = "🟡 🟣 🔴 : 黄 紫 赤" ] || ng "$LINENO"
 
 # 異常値テスト
-out=$(echo "あ" | ./resistor)
-[ "${out}" = "数値を入力してください" ] || ng "$LINENO"
+echo "あ" | ./resistor > /dev/null 2>&1
+if [ "$?" -ne 1 ]; then
+    ng "$LINENO"
+fi
 
 [ "$res" = 0 ] && echo OK
 exit $res
